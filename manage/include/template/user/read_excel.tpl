fed1d38cde0d9281ada754b887e6e87bf77b64e5
kingdragon <kingdragon>
2017-04-04 17:06:44 +0800

    batch add books, questions

--- manage/include/template/user/read_excel.tpl
+++ manage/include/template/user/read_excel.tpl
@@ -3,22 +3,22 @@
 <{include file ="sidebar.tpl"}>
 <!-- TPLSTART 以上内容不需更改，保证该TPL页内的标签匹配即可 -->
 <div class="block">
-	<a href="#page-stats" class="block-heading" data-toggle="collapse">读取Excel.xls</a>
+	<a href="#page-stats" class="block-heading" data-toggle="collapse"><{$title}></a>
 	<div id="page-stats" class="block-body collapse in">
-	
-	<form id="tab" method="post" action="" autocomplete="off" ENCTYPE="multipart/form-data">
-				<hr />
-				<label>只限于xls文件</label>
-				<input type="file" name="excel"  id="DropDownTimezone"  class="input-xlarge">
-				 
-				<div class="btn-toolbar">
-				<button type="submit" class="btn btn-primary"><strong>提交</strong></button>
-				<div class="btn-group"></div>
-				</div>
+		<form id="tab" method="post" action="" autocomplete="off" ENCTYPE="multipart/form-data">
+			<hr />
+			<label>只限于xls文件</label>
+			<input type="file" name="excel"  id="DropDownTimezone"  class="input-xlarge">
+
+			<div class="btn-toolbar">
+			<input type="hidden" name="form_token" value="<{$form_token}>" >
+			<button type="submit" class="btn btn-primary"><strong>提交</strong></button>
+			<div class="btn-group"></div>
+			</div>
 		</form>
 		<pre>
-		读取Excel.xls文件输出结果
-		<{$output}>
+			<{$title}>结果
+			<{$output}>
 		</pre>
 	</div>
 </div>
--- manage/user/read_excel.php
+++ manage/user/read_excel.php
@@ -1,6 +1,32 @@
 <?php
 require('../include/init.inc.php');
-if (Common::isPost ()) {
+
+$form_token = $type = '';
+extract ( $_REQUEST, EXTR_IF_EXISTS );
+// var_dump($_FILES);
+$title = $excel_array = '';
+$token = $_SESSION['batch_upload_token'];
+
+switch ($type) {
+	case 'book':
+		$title = '批量添加图书';
+		break;
+
+	case 'objques':
+		$title = '批量添加客观题';
+		break;
+
+	case 'subques':
+		$title = '批量添加主观题';
+		break;
+
+	default:
+		$title = '';
+		break;
+}
+
+if (Common::isPost () && checkToken($token, $form_token)) {
+	// var_dump('post');
 	if (empty($_FILES['excel'])) {
 		OSAdmin::alert('error', 'empty file');
 	} else {
@@ -9,12 +35,198 @@ if (Common::isPost ()) {
 			OSAdmin::alert("error",$message);
 		}
 		$file = $_FILES['excel']['tmp_name'];
+		// var_dump($file);
 		$excel_array = ExcelReader::readXLS($file);
 
-		$output = print_r($excel_array,true);
+		// var_dump($excel_array);
+		switch ($type) {
+			case 'book':
+				$res = batchAddBooks($excel_array);
+				break;
+
+			case 'objques':
+				$res = batchAddObjQues($excel_array);
+				break;
+
+			case 'subques':
+				$res = batchAddSubQues($excel_array);
+				break;
+
+			default:
+				$res = '';
+				break;
+		}
+
+		// var_dump($res);
+		// $output = print_r($excel_array,true);
+		$output = print_r($res, true);
+		// Template::assign("title", $title);
+		// // Template::assign("_POST", $_POST);
+		// Template::assign("output", $output);
+		// Template::display('user/read_res.tpl');
+		// Common::exitWithSuccess ( '已删除','book/books.php' );
+		// exit();
+		// $_REQUEST = array();
+		unset($_SESSION['batch_upload_token']);
+		// $form_token
+		// Template::assign("output", $output);
+		// Template::display('user/read_excel.tpl');
+		// $_SESSION['batch_upload_token'] = '';
 	}
 }
+else {
+	$form_token = time();
+	$_SESSION['batch_upload_token'] = generateToken($form_token);
+}
 
-Template::assign("_POST", $_POST);
+// var_dump($token);
+// Template::assign("title", $title);
+Template::assign("form_token", $form_token);
+Template::assign("title", $title);
+// Template::assign("_POST", $_POST);
 Template::assign("output", $output);
 Template::display('user/read_excel.tpl');
+
+
+function batchAddBooks($batch_data)
+{
+	// $ret = array();
+	$suc = 0;
+	$err = 0;
+	$fail = array();
+
+	$len = count($batch_data);
+	if (empty($batch_data)) {
+		return '';
+	}
+	if ($len < 2) {
+		return '无内容添加';
+	}
+	for ($i=1; $i < $len; $i++) {
+		$info = $batch_data[$i];
+		$data = array(
+			// 'id' => 1,
+			'name' => $info[0],
+			'author' => $info[1],
+			'press' => $info[2],
+			'presstime' => strtotime($info[3]),
+			'bookdesc' => $info[4],
+			'coverimg' => $info[5],
+			'type' => $info[6],
+			'score' => $info[7],
+			'level' => $info[8],
+			'grade' => $info[9],
+			'wordcount' => $info[10],
+			'addtime' => time(),
+		);
+		$res = Book::addBook($data);
+		if ($res) {
+			$suc++;
+		}
+		else {
+			$err++;
+			$fail[] = implode(',', $data);
+		}
+		// var_dump($data);
+	}
+	// $ret = '成功添加'.$suc;
+	return composeMsg($suc, $err, $fail);
+}
+
+
+function batchAddObjQues($batch_data)
+{
+	// $ret = array();
+	$suc = 0;
+	$err = 0;
+	$fail = array();
+
+	$len = count($batch_data);
+	if (empty($batch_data)) {
+		return '';
+	}
+	if ($len < 2) {
+		return '无内容添加';
+	}
+	for ($i=1; $i < $len; $i++) {
+		$info = $batch_data[$i];
+		$data = array(
+			// 'id' => 1,
+			'book_id' => $info[0],
+			'question' => $info[1],
+			'choice1' => $info[2],
+			'choice2' => $info[3],
+			'choice4' => $info[4],
+			'answer' => $info[5],
+			'view' => $info[6],
+			'addtime' => time(),
+		);
+		$res = Question::addInfo($data);
+		if ($res) {
+			$suc++;
+		}
+		else {
+			$err++;
+			$fail[] = implode(',', $data);
+		}
+		// var_dump($data);
+	}
+	// $ret = '成功添加'.$suc;
+	return composeMsg($suc, $err, $fail);
+}
+
+
+function batchAddSubQues($batch_data)
+{
+	// $ret = array();
+	$suc = 0;
+	$err = 0;
+	$fail = array();
+
+	$len = count($batch_data);
+	if (empty($batch_data)) {
+		return '';
+	}
+	if ($len < 2) {
+		return '无内容添加';
+	}
+	for ($i=1; $i < $len; $i++) {
+		$info = $batch_data[$i];
+		$data = array(
+			// 'id' => 1,
+			'book_id' => $info[0],
+			'question' => $info[1],
+			'addtime' => time(),
+		);
+		$res = Objques::addInfo($data);
+		if ($res) {
+			$suc++;
+		}
+		else {
+			$err++;
+			$fail[] = implode(',', $data);
+		}
+		// var_dump($data);
+	}
+	// $ret = '成功添加'.$suc;
+	return composeMsg($suc, $err, $fail);
+}
+
+
+function composeMsg($suc, $err, $fail)
+{
+	$msg = '<br>添加成功'.$suc.'条<br><br>';
+	$msg .= '添加失败'.$err.'条<br><br>';
+	$msg .= implode('<br>', $fail);
+	return $msg;
+}
+
+function checkToken($token, $form_token)
+{
+	return $token == generateToken($form_token);
+}
+
+function generateToken($form_token)
+{
+	return md5($_SESSION['osa_verify_code'].$form_token);
+}
